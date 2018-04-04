0766d9bd1a74ab6c3fa1c08480cbdc826970e73e
Mary Jack <maryjackinthebox@gmail.com>
2018-04-04 00:11:08 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,15 @@
 def unsafe?(speed)
-
+  if speed > 60 || speed < 40 
+    return true 
+  else
+    return false
+  end
 end
-
 
 
 def not_safe?(speed)
-	
+	speed > 60 || speed < 40 ?  true : false
 end
 	
 
-
+#I initially had return true and return false. The return was redundant. Code ran when I removed return
